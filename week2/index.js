36f509e00172b61e41eb3ed2b9be60be544fccc3
vzts <yeechan.ko@gmail.com>
2017-12-31 17:40:11 +0800

    week2 complete toggle done

--- week2/index.js
+++ week2/index.js
@@ -3,12 +3,21 @@
     var todoInput = $("#input-box input");
     var todoTemplate = '<div class="todo-item"><input type="checkbox"><div class="todo-content">{{content}}</div></div>'
 
+    var todoCheckbox = $(".todo-item input");
+
     todoInput.keyup(function (event) {
         if (event.which === 13 || event.which === 108) {
             var content = todoInput.val();
             var todo = createTodo(content);
             todoList.append(todo);
             todoInput.val("");
+        }
+    });
+
+    todoList.click(function(event){
+        if(event.target.nodeName === "INPUT") {
+            var todoContent = $($(event.target).next());
+            todoContent.toggleClass("completed");
         }
     });
 
